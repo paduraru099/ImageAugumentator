f28c9967d709d5c07f5bd5f0e1803b0ffb896335
Razvan Paduraru <Paduraru.Gh.Razvan@utcluj.didatec.ro>
2021-05-17 23:27:16 +0300

    updates

--- FrontendAugmentator-main/src/App.tsx
+++ FrontendAugmentator-main/src/App.tsx
@@ -1,9 +1,35 @@
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import React from 'react';
 import './App.css';
 import { AppRoutes } from './Routes/AppRoutes';
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      "Montserrat",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+
+    fontWeightMedium: "bolder",
+    fontSize: 14,
+  },
+
+  
+});
+
 function App() {
   return (
-    <AppRoutes/>
+    <ThemeProvider theme={theme}>
+      <AppRoutes/>
+    </ThemeProvider>
   );
 }
 
